53bebb6d1c3136cca2d6f5aa84cb01b0b3805c82
Wallace Tácio <wallacett.dev@gmail.com>
2021-11-03 09:02:26 -0300

    Adiciona resolu~

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Negativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num;
+
+            Console.Write("Digite um número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if(num < 0){
+                Console.Write("Você digitou um número ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("negativo.");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.Write("O número que você digitou ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("não é negativo.");
+                Console.ResetColor();
+
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Negativo
-Exercício Negativo ETEC 2021
+## Exercício `Negativo` ETEC 2021
+
+Leia um número inteiro e exiba a mensagem "Você digitou um número negativo." caso o número seja menor que zero.
+
+Ex.:
+```
+Digite um número: -12
+Você digitou um número negativo.
+```
+ou
+```
+Digite um número: 12
+```
+_(nada a exibir)_
